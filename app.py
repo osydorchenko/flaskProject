e3a6c0ffe609ad3d4b0f89568149c9d21ec1f99f
osydorchenko <ljubljana.sasha@gmail.com>
2021-06-27 13:10:38 +0300

    adding list

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    data = ['1', '2', '3', '4', '5', '6', '7']
+    return render_template('index.html',
+                           name=request.args["name"],
+                           lastname=request.args["lastname"],
+                           data=data,
+                           flag=False)
 
 
 if __name__ == '__main__':
