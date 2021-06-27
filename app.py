faaef9706137386702504f6ee39f313eb1e47a68
osydorchenko <ljubljana.sasha@gmail.com>
2021-06-27 13:44:47 +0300

    adding pages Main & Data

--- app.py
+++ app.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     data = ['1', '2', '3', '4', '5', '6', '7']
-    return render_template('index.html',
-                           name=request.args["name"],
-                           lastname=request.args["lastname"],
-                           data=data,
-                           flag=False)
+    if 'name' in request.args:
+        return render_template('index.html',
+                               name=request.args["name"],
+                               lastname=request.args["lastname"],
+                               data=data,
+                               flag=False)
+    else:
+        return render_template('index.html',
+                               name='Olena',
+                               lastname='Bdzhilka',
+                               data=data,
+                               flag=False)
+
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
 
 
 if __name__ == '__main__':
